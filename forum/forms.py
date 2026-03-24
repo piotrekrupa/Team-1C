74f71849061d64eb08e8d2721cdfcf9055efaddc
“Piotr <3023485K@student.gla.ac.uk>
2026-03-24 15:08:08 +0000

    Styling of Hompage and upload page (unfinished 1)

--- forum/forms.py
+++ forum/forms.py
@@ -9,13 +9,13 @@ class ListingForm(forms.ModelForm):
         fields = ['title', 'company', 'location', 'job_type', 'deadline_date', 'application_url', 'description']
 
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control custom-input'}),
-            'company': forms.TextInput(attrs={'class': 'form-control custom-input'}),
-            'location': forms.TextInput(attrs={'class': 'form-control custom-input', 'placeholder': 'e.g. London, Remote'}),
-            'job_type': forms.Select(attrs={'class': 'form-select custom-input'}),
-            'deadline_date': forms.DateInput(attrs={'class': 'form-control custom-input', 'type': 'date'}),
-            'application_url': forms.URLInput(attrs={'class': 'form-control custom-input', 'placeholder': 'https://...'}),
-            'description': forms.Textarea(attrs={'class': 'form-control custom-input', 'rows': 3}),
+            'title': forms.TextInput(attrs={'class': 'form-control upload-input'}),
+            'company': forms.TextInput(attrs={'class': 'form-control upload-input'}),
+            'location': forms.TextInput(attrs={'class': 'form-control upload-input', 'placeholder': 'e.g. London, Remote'}),
+            'job_type': forms.Select(attrs={'class': 'form-select upload-input'}),
+            'deadline_date': forms.DateInput(attrs={'class': 'form-control upload-input', 'type': 'date'}),
+            'application_url': forms.URLInput(attrs={'class': 'form-control upload-input', 'placeholder': 'https://...'}),
+            'description': forms.Textarea(attrs={'class': 'form-control upload-input', 'rows': 3}),
         }
 
 class SignUpForm(UserCreationForm):
